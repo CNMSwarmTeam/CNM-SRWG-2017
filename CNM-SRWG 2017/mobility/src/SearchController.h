7475588fe5467717197c982ee5102dc568e75831
Jeff Schlindwein <jeff.schlindwein@gmail.com>
2017-03-23 01:03:47 -0600

    Update SearchController.h

--- CNM-SRWG 2017/mobility/src/SearchController.h
+++ CNM-SRWG 2017/mobility/src/SearchController.h
@@ -10,85 +10,90 @@
  * here should be modified and enhanced to improve search performance.
  */
 
-
-
 class SearchController
 {
 
   public:
 
+    //METHODS
+    //--------------------------------------
+
+    //CONSTRUCTOR
     SearchController();
 
-    //sets centerSeen bool
-    void setCenterSeen(bool answer);
+    //GETS
+    bool getCenterSeen(){ return cnmCenterSeen; }
 
-    void setCenterLocation(geometry_msgs::Pose2D newLocation);
-
-    void cnmSetRotations(int num);
-
-    int cnmGetSearchPosition();
-    double cnmGetSearchDistance();
+    bool getHasDoneRotation() { return doneOneFullRotation; }
 
     bool cnmIsAlternating() { return reverseSearch; }
 
-    // performs search pattern
-    geometry_msgs::Pose2D search(geometry_msgs::Pose2D currentLocation);
+    int cnmGetSearchPosition();
 
-    // continues search pattern after interruption
-    //geometry_msgs::Pose2D continueInterruptedSearch(geometry_msgs::Pose2D currentLocation, geometry_msgs::Pose2D oldGoalLocation);
-    geometry_msgs::Pose2D continueInterruptedSearch(geometry_msgs::Pose2D currentLocation, geometry_msgs::Pose2D oldGoalLocation);
+    double cnmGetSearchDistance();
 
-    bool getCenterSeen(){ return cnmCenterSeen; }
-
-    void obstacleWasAvoided();
-
-    bool getHasDoneRotation() { return hasDoneRotation; }
-
+    //SETS
     bool doAnotherOctagon() { doneOneFullRotation = false; }
 
     void AmILost(bool answer);
 
+    void setCenterSeen(bool answer);							//sets centerSeen bool
+
+    void setCenterLocation(geometry_msgs::Pose2D newLocation);
+
+    void obstacleWasAvoided(){ avoidedObstacle = true; numTimesAvoidedObst++; }
+
+    //SEARCH ALOGIRTHM CALLS
+    geometry_msgs::Pose2D search(geometry_msgs::Pose2D currentLocation);		// performs search pattern
+
+    geometry_msgs::Pose2D continueInterruptedSearch(geometry_msgs::Pose2D currentLocation, geometry_msgs::Pose2D oldGoalLocation);	// continues search pattern after interruption
+
   private:
-    //VARIABLES
-    //--------------------------------------
-    random_numbers::RandomNumberGenerator* rng;
-
-    //CNM VARIABLES
-    //--------------------------------------
-    geometry_msgs::Pose2D cnmCenterLocation;
-
-    //BOOLEAN TRIGGERS
-    //--------------------------------------
-    bool cnmCenterSeen;
-    bool cnmHasReset;
-    bool avoidedObstacle;
-
-    bool hasDoneRotation;
-    bool doneOneFullRotation;
 
     //PRIMITIVES
     //--------------------------------------
 
     //Target Search
-    int searchLoop;             //int of 0 - 8
+    int searchLoop;             	//int of 0 - 9 for search positions
+    int minNumRotations;		//MINIMUM number of rotations before we consider it having done enough of an octagon to increment
+    int cnmNumRotations;		//Number of times the rover has gone to next point before hitting point 0
+    int numTimesAvoidedObst;		//Number of times rover had to avoid an obstacle before eventually moving on to a new point
 
-    double searchDist;
-    double searchCounter;
-    double cnmSearchCounterDistance;
+    double searchDist;			//Distance to add to search distance when incrementing (added to searchCounter)
+    double searchCounter;		//Distance to search from center
+    double cnmSearchCounterDistance;	//Stores ORIGINAL search distance value
 
-    bool reverseSearch;
 
-    int cnmNumRotations;
+    //BOOLEAN TRIGGERS
+    bool cnmCenterSeen;			//If we have seen the center yet
+    bool avoidedObstacle;		//Triggered true by obstacleWasAvoided
 
-    //gets closest point to go to
-    geometry_msgs::Pose2D cnmGetPositionInSearchPattern(geometry_msgs::Pose2D currentLocation);
+    bool doneFirstRotation;		//If we have finished our first rotation.  Implemented so we always complete ONE FULL LOOP
+    bool doneOneFullRotation;		//If we have managed to finish another full rotation
 
-    int RightSearch(double absLocationAngle);
-    int LeftSearch(double absLocationAngle);
+    bool reverseSearch;			//If we are going to alternate from left and right
+
+    //OTHER VARIABLES
+    //--------------------------------------
+    random_numbers::RandomNumberGenerator* rng;
+    geometry_msgs::Pose2D cnmCenterLocation;
+
+    //METHODS
+    //--------------------------------------
+
+    void RightSearch(double absLocationAngle);
+    void LeftSearch(double absLocationAngle);\
+
+    void checkIfIncrementing();
+
 
     //performs alternating left/right search pattern
     geometry_msgs::Pose2D SearchLeft(geometry_msgs::Pose2D currentLocation);
     geometry_msgs::Pose2D SearchRight(geometry_msgs::Pose2D currentLocation);
+
+    //gets closest point to go to
+    geometry_msgs::Pose2D cnmGetPositionInSearchPattern(geometry_msgs::Pose2D currentLocation);
+
 };
 
 #endif /* SEARCH_CONTROLLER */
