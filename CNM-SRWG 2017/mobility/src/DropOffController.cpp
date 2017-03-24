46079a36cc90233477d0958d966f6b08e5462518
Jeff Schlindwein <jeff.schlindwein@gmail.com>
2017-03-23 21:14:33 -0600

    Update DropOffController.cpp

--- CNM-SRWG 2017/mobility/src/DropOffController.cpp
+++ CNM-SRWG 2017/mobility/src/DropOffController.cpp
@@ -9,38 +9,7 @@ DropOffController::DropOffController()
     centeringTurn = 0.15;                   //radians
     seenEnoughCenterTagsCount = 13;
     collectionPointVisualDistance = 0.50;   //in meters
-    reachedCollectionPoint = false;
-    spinSize = 0.10;                        //in meters aka 10cm
-    addSpinSizeAmmount = 0.10;              //in meters
-
-    result.cmdVel = 0;
-    result.angleError = 0;
-    result.fingerAngle = -1;
-    result.wristAngle = -1;
-    result.goalDriving = false;             //set true for when driving is goal location oriented and false when it is target or time oriented
-    result.centerGoal;                      //goal that is center location or based upon the center location.
-    result.reset = false;
-    result.timer = false;
-
-    left = false;
-    right = false;
-
-    circularCenterSearching = false;
-    spinner = 0;
-    centerApproach = false;
-    timeWithoutSeeingEnoughCenterTags = time(0);
-    seenEnoughCenterTags = false;
-    centerSeen = false;
-    timeElapsedSinceTimeSinceSeeingEnoughCenterTags = time(0);
-    circularCenterSearching = false;
-    prevCount = 0;
-
-    searchVelocity = 0.15;
-
-    isLost = false;
 */
 }
 
-DropOffController::~DropOffController() {
-
-}
+DropOffController::~DropOffController() { }
