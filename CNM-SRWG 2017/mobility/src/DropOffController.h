763efe178d87c843fd3c93332c8c7675a84a1439
Jeff Schlindwein <jeff.schlindwein@gmail.com>
2017-03-23 21:10:19 -0600

    Update DropOffController.h

--- CNM-SRWG 2017/mobility/src/DropOffController.h
+++ CNM-SRWG 2017/mobility/src/DropOffController.h
@@ -6,86 +6,14 @@
 #include <std_msgs/Float32.h>
 #include "SearchController.h"
 
-struct DropOffResult {
-    float cmdVel;
-    float angleError;
-    float fingerAngle;
-    float wristAngle;
-    bool goalDriving; //set true for when driving is goal location oriented and false when it is target or time oriented
-    geometry_msgs::Pose2D centerGoal;
-    bool reset;
-    bool timer;
-};
-
 class DropOffController
 {
+
 public:
     DropOffController();
-
     ~DropOffController();
 
-    void reset();
-
-    DropOffResult getState() {return result;}
-    float getSpinner() {return spinner;}
-    float getCentX() { return centerLocation.x;}
-    float getCount() { return count;}
-    bool getCenterApproach() { return centerApproach;}
-
-    void setDataTargets(int ccount, double lleft, double rright);
-    void setCenterDist(float dist) {distanceToCenter = dist;}
-    void setDataLocations(geometry_msgs::Pose2D center, geometry_msgs::Pose2D current, float sync);
-
-    //void setSearch(SearchController *cont) { DropOffSearch = cont; }
-
-    bool cnmInPosition;
-
 private:
-
-    bool right;
-    bool left;
-    bool reachedCollectionPoint;
-
-    //is driving in a circle to find the nest
-    bool circularCenterSearching;
-
-    //keep track of progression around a circle when driving in a circle
-    float spinner;
-
-    //set to true when we are entering the center circle
-    bool centerApproach;
-
-    //we have seen enough central collection tags to be certain we are either in or driving towards the nest.
-    bool seenEnoughCenterTags;
-
-    //central collection point has been seen (aka the nest)
-    bool centerSeen;
-
-    time_t timeWithoutSeeingEnoughCenterTags;
-    float cameraOffsetCorrection;
-    float centeringTurn;
-    int seenEnoughCenterTagsCount;
-    DropOffResult result;
-    int count;
-    int prevCount;
-    double countLeft;
-    double countRight;
-    float collectionPointVisualDistance;
-    float distanceToCenter;
-    geometry_msgs::Pose2D centerLocation;
-    geometry_msgs::Pose2D currentLocation;
-    float timerTimeElapsed;
-    time_t timerStartTime;
-    float timeElapsedSinceTimeSinceSeeingEnoughCenterTags;
-    float spinSize;
-    float addSpinSize;
-    float addSpinSizeAmmount;
-
-    //SearchController DropOffSearch;
-
-    float searchVelocity;
-
-    void calculateDecision();
 
 };
 #endif // end header define
